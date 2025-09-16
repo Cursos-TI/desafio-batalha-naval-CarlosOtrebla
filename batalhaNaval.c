300fed3e53110ab3d3064cf6996af8ce6ca1dfd3
CarlosOtrebla <carlosotrebla@gmail.com>
2025-09-16 00:04:11 -0300

    Adicionado o quarto navio

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define SIZE 12
+#define SIZE 10
 
 void printBoard(int board[SIZE][SIZE])
 {
@@ -19,14 +19,14 @@ int main()
     // Definindo o tabuleiro
     int board[SIZE][SIZE] = {0};
 
-    // Posição do navio 1
+    // Navio 1
     int ship_1[3][2] = {{0, 2}, {1, 2}, {2, 2}};
-
-    // Posição do navio 2
+    // Navio 2
     int ship_2[3][2] = {{8, 3}, {8, 4}, {8, 5}};
-
-    // Posição do navio 3
+    // Navio 3
     int ship_3[3][2] = {{3, 5}, {4, 6}, {5, 7}};
+    // Navio 4
+    int ship_4[3][2] = {{7, 1}, {6, 2}, {5, 3}};
 
     // Posicionando os navios
     for (int i = 0; i < 3; i++)
@@ -34,9 +34,8 @@ int main()
         board[ship_1[i][0]][ship_1[i][1]] = 3;
         board[ship_2[i][0]][ship_2[i][1]] = 3;
         board[ship_3[i][0]][ship_3[i][1]] = 3;
+        board[ship_4[i][0]][ship_4[i][1]] = 3;
     }
-
-   
 
     printBoard(board);
 
